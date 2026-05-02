7e878a4fd604ce5d88413aeaf921362c958a0d44
Ian Zuñiga <ian.zuniga@Ian-Zuniga.lan>
2026-05-01 18:08:57 -0600

    feat: Synchronize user routine with exercise store

--- src/store/excersiceStore.tsx
+++ src/store/excersiceStore.tsx
@@ -1,13 +1,13 @@
 import { create } from "zustand";
-import type { RoutineEntry } from "../types/core";
-
-interface ExerciseState {
-  entries: RoutineEntry[];
-  addExercise: (entry: RoutineEntry) => void;
-}
+import type { RoutineEntry, ExerciseState } from "../types/core";
+import { useUserStore } from "./userStore";
 
 export const useExerciseStore = create<ExerciseState>((set) => ({
   entries: [],
-  addExercise: (entry: RoutineEntry) =>
-    set((state) => ({ entries: [...state.entries, entry] })),
+  addExercise: (entry: RoutineEntry): void =>
+    set((state: ExerciseState) => {
+      const updatedEntries = [...state.entries, entry];
+      useUserStore.getState().updateUserRoutine(updatedEntries);
+      return { entries: updatedEntries };
+    }),
 }));
